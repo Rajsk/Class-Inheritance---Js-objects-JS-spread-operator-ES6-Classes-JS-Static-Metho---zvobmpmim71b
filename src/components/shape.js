2f2bd7116434c15d02dd527246af3d924556ad47
Rajsk <rakibulsk.cse@gmail.com>
2023-01-06 18:18:25 +0530

    add color

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
-// export class using module.exports
+
+class Shape {
+    constructor(color) {
+      if (color) {
+          this.color = color;
+      } else {
+          this.color = 'red';
+      }
+    }
+    drawShape() {}
+  
+    calculateArea() {}
+  }
+  
+  // export class using module.exports
+  module.exports = Shape;
